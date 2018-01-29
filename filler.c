c4d2186e870dda60817bbdc284fb59ca54f13d82
Razshal <fontenille.mathieu@gmail.com>
2018-01-29 17:05:04 +0100

    fixed a misunderstanding with x and y axis norms

--- filler.c
+++ filler.c
@@ -6,7 +6,7 @@
 /*   By: mfonteni <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/24 11:58:25 by mfonteni          #+#    #+#             */
-/*   Updated: 2018/01/29 16:10:51 by mfonteni         ###   ########.fr       */
+/*   Updated: 2018/01/29 17:03:59 by mfonteni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,22 +17,23 @@ int place_piece(t_fill *infos, t_coord place, int line, int row)
 	int res;
 
 	res = 0;
-	printf("line:%d, row:%d\n", line + place.x, row + place.y);
+	printf("line:%d, row:%d\n", line + place.y, row + place.x);
 	if (!infos->currentpiece[line] && infos->overflow == 1)
 		return (1);
-	else if (!infos->currentpiece[line] && infos->overflow != 1)
+	else if ((!infos->currentpiece[line] && infos->overflow != 1) ||
+			(infos->currentpiece[line][row] == '*' && line + place.y < 0) ||
+			(infos->currentpiece[line][row] == '*' && row + place.x < 0))
 		return (0);
 	if (infos->currentpiece[line][row] == '*' &&
-			infos->grid[line + place.x][row + place.y] == infos->player)
+			infos->grid[line + place.y][row + place.x] == infos->player)
 		infos->overflow++;
-	if (line + place.x > infos->gridsize.x || !infos->grid[line + place.x] ||
-			!infos->grid[line + place.x][row + place.y] || infos->overflow > 1)
+	if (line + place.y > infos->gridsize.y || infos->overflow > 1)
 		return (0);
-	if (row < infos->piecesize.y)
+	if (row < infos->piecesize.x)
 		res = place_piece(infos, place, line, ++row);
-	else if (line < infos->piecesize.x)
+	else if (line < infos->piecesize.y)
 		res = place_piece(infos, place, ++line, 0);
-	if (res == 1 && infos->grid[line + place.x][row + place.y] == '.')
+	if (res == 1 && infos->grid[line + place.y][row + place.x] == '.')
 		return (1);
 	else if (res == 0)
 		return (0);
--- parsers.c
+++ parsers.c
@@ -6,7 +6,7 @@
 /*   By: mfonteni <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/25 16:31:46 by mfonteni          #+#    #+#             */
-/*   Updated: 2018/01/26 15:24:02 by mfonteni         ###   ########.fr       */
+/*   Updated: 2018/01/29 17:02:28 by mfonteni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,14 +20,14 @@ void	set_grid_size(t_fill *infos)
 	count = 0;
 	while (!ft_strstr(infos->currentline, "Plateau"))
 		get_next_line(0, &infos->currentline);
-	infos->gridsize.x =
+	infos->gridsize.y =
 		ft_atoi((temp = ft_strstr(infos->currentline, "Plateau")));
 	temp++;
 	while (ft_isdigit(temp[count]))
 		count++;
 	while (ft_ispace(temp[count]))
 		count++;
-	infos->gridsize.y = ft_atoi(&temp[count]);
+	infos->gridsize.x = ft_atoi(&temp[count]);
 }
 
 void	set_piece_size(t_fill *infos)
@@ -38,14 +38,14 @@ void	set_piece_size(t_fill *infos)
 	count = 0;
 	while (!ft_strstr(infos->currentline, "Piece"))
 		get_next_line(0, &infos->currentline);
-	infos->piecesize.x = ft_atoi((temp =
+	infos->piecesize.y = ft_atoi((temp =
 			ft_strstr(infos->currentline, "Piece")));
 	temp++;
 	while (ft_isdigit(temp[count]))
 		count++;
 	while (ft_ispace(temp[count]))
 		count++;
-	infos->piecesize.y = ft_atoi(&temp[count]);
+	infos->piecesize.x = ft_atoi(&temp[count]);
 }
 
 void	grid_parser(t_fill *infos)
@@ -53,14 +53,14 @@ void	grid_parser(t_fill *infos)
 	int		line;
 
 	line = 0;
-	if (infos->gridsize.x == -1)
+	if (infos->gridsize.y == -1)
 		set_grid_size(infos);
-	if (!(infos->grid = (char**)malloc(sizeof(char*) * infos->gridsize.x + 2)))
+	if (!(infos->grid = (char**)malloc(sizeof(char*) * infos->gridsize.y + 2)))
 		return ;
-	while (line < infos->gridsize.x)
+	while (line < infos->gridsize.y)
 	{
 		if (!infos->grid[line])
-			infos->grid[line] = ft_strnew(infos->gridsize.y + 1);
+			infos->grid[line] = ft_strnew(infos->gridsize.x + 1);
 		if (infos->grid[line] && ft_isdigit((infos->currentline)[0]))
 			ft_strcat(infos->grid[line++], &(infos->currentline)[4]);
 		else
@@ -79,10 +79,10 @@ void	piece_parser(t_fill *infos)
 	if (!(infos->currentpiece
 				= (char**)malloc(sizeof(char*) * infos->piecesize.x + 2)))
 		return ;
-	while (line < infos->piecesize.x)
+	while (line < infos->piecesize.y)
 	{
 		if (!infos->currentpiece[line])
-			infos->currentpiece[line] = ft_strnew(infos->piecesize.y + 1);
+			infos->currentpiece[line] = ft_strnew(infos->piecesize.x + 1);
 		if (infos->currentpiece[line] && !ft_strstr(infos->currentline, "<got"))
 			ft_strcat(infos->currentpiece[line++], infos->currentline);
 		else
