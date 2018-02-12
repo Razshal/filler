6769f55658f664aa15328f5db4e9e34040332884
Razshal <fontenille.mathieu@gmail.com>
2018-02-12 12:10:52 +0100

    no more segfault on placer

--- placer.c
+++ placer.c
@@ -6,7 +6,7 @@
 /*   By: mfonteni <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/29 18:18:15 by mfonteni          #+#    #+#             */
-/*   Updated: 2018/02/09 18:54:27 by mfonteni         ###   ########.fr       */
+/*   Updated: 2018/02/12 12:10:16 by mfonteni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,51 +14,29 @@
 
 static int	valid_placement(t_fill *infos, t_coord place, int line, int row)
 {
-	if (infos->currentpiece[line][row] == '*'
-			&& !infos->grid[line + place.y])
-		return (0);
-
-	if (line > infos->piecesize.y
-			|| row > infos->piecesize.x
-			|| ((infos->currentpiece[line][row] == '*')))
+	if (infos->currentpiece[line][row] == '*')
 	{
-		if (line + place.y < 0
-				|| line + place.y > infos->gridsize.y
+		if (line > infos->piecesize.y
+				|| row > infos->piecesize.x
+				|| line + place.y < 0
 				|| row + place.x < 0
-				|| row + place.x > infos->gridsize.x
-				|| (infos->grid[line + place.y][row + place.x] != '.'
-				&& infos->grid[line + place.y][row + place.x] != infos->player))
+				|| line + place.y > infos->gridsize.y
+				|| row + place.x > infos->gridsize.x)
+			return (0);
+ 		if (!infos->grid[line + place.y])
+			return (0);
+		if (infos->grid[line + place.y][row + place.x] != '.'
+				&& infos->grid[line + place.y][row + place.x] != infos->player)
 			return (0);
 	}
 	return (1);
 }
-
-/*
-static void	whereami(t_fill *infos, t_coord pos)
-{
-	char c;
-	static int i = 0;
-	if (pos.y < infos->gridsize.y && pos.x < infos->gridsize.x && pos.y >= 0 && pos.x >= 0)
-	{
-		c = infos->grid[pos.y][pos.x];
-		dprintf(FD, "loop n%d\n", i++);
-		infos->grid[pos.y][pos.x] = 'A';
-		ft_print_split_fd(infos->grid, FD);
-	}
-	else
-		c = -1;
-	if (c > -1)
-		infos->grid[pos.y][pos.x] = c;
-	usleep(10000);
-}
-*/
 
 int		place_piece(t_fill *infos, t_coord place, int line, int row)
 {
 	int res;
 
 	res = 0;
-//	whereami(infos, place);
 	if (!infos->currentpiece[line] && infos->overflow == 1)
 	{
 		infos->place.x = place.x;
